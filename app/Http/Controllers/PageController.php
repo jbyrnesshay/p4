0f81d5064a16e9022b3b35f734421801a3d88d3a
jbyrnesshay <jmb464@g.harvard.edu>
2016-12-12 03:26:16 -0500

    added lockdown of /home, unless $user, welcome page planned for /

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Picnook\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function welcome(Request $request) {
+
+    # Logged in users should not see the welcome page, send them to the books index instead.
+    if($request->user()) {
+        return redirect('/home');
+    }
+
+    else return view('welcome');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+ 
+Route::get('/', 'PageController@welcome'
+)->name('welcome');
 
-Route::get('/', 'PicController@index'
-)->name('home');
+Route::get('/home', 'PicController@index'
+)->name('home')->middleware('auth');
  
 Route::get('/create/{item}', 'PicController@create')->name('create');
 
@@ -21,6 +24,6 @@ Route::post('/store', 'PicController@store')->name('store');
 
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 Route::get("/register", 'Auth\RegisterController@register')->name('register');
-Route::get('/welcome', 'HomeController@index');
+Route::get('/welcome', 'HomeController@index')->name('welcome');
 
 Auth::routes();
